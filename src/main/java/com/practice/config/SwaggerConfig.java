f9754a7470a807d8550d3258038299d3f02d3aaa
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-03-01 16:54:57 -0500

    fix: add router swagger

--- src/main/java/com/practice/config/SwaggerConfig.java
+++ src/main/java/com/practice/config/SwaggerConfig.java
@@ -1,21 +1,53 @@
 package com.practice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpHeaders;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "Documentacion de Api Microservicio de Clients",
-                description = "Documentacion de Api Microservicio de  Clients",
-                version = "1.0.0"
+                description = "Documentacion de Api Microservicio de Clients",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Microservicio Clients",
+                        url = "http://localhost:9090"
+                ),
+                license = @License(
+                        name = "Standard Apache License Version 2.0 for Fintech",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0",
+                        identifier = "Apache-2.0"
+                )
         ),
         servers = {
                 @Server(
                         description = "Local Server",
-                        url = "http://localhost:8080/api/v1/client"
+                        url = "http://localhost:9090/api/v1/client"
+                ),
+                @Server(
+                        description = "Production Server",
+                        url = "https://fatal-felicity-lourdes-74626af5.koyeb.app"
                 )
-        }
+        },
+        security = @SecurityRequirement(
+                name = "securityToken"
+        )
+)
+@SecurityScheme(
+        name = "securityToken",
+        description = "Access Token For My API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class SwaggerConfig {
 
